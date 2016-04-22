b82d5ab6845a89c9fc2905c17f0632f2ee7e589a
Nick Embrey <cne.embrey@gmail.com>
2016-04-22 08:39:36 -0700

    Fail 2nd test runWordPuzzle_replaceAllFiveVowelswithFiveDashes_String.

--- src/test/java/WordPuzzleTest.java
+++ src/test/java/WordPuzzleTest.java
@@ -5,13 +5,19 @@ import java.util.ArrayList;
 public class WordPuzzleTest{
 
   // Test passes a single vowel into runWordPuzzle and expects it to return a single dash.
-
   @Test
   public void runWordPuzzle_replaceVowelwithDash_String() {
   WordPuzzle testWordPuzzle = new WordPuzzle();
   String expected = "-";
-
   assertEquals(expected, testWordPuzzle.runWordPuzzle("a"));
+  }
+
+  // Test passes a string of all five vowels into runWordPuzzle and expects it to return a string of five dashes.
+  @Test
+  public void runWordPuzzle_replaceAllFiveVowelswithFiveDashes_String() {
+  WordPuzzle testWordPuzzle = new WordPuzzle();
+  String expected = "-----";
+  assertEquals(expected, testWordPuzzle.runWordPuzzle("aeiou"));
   }
 
 }
